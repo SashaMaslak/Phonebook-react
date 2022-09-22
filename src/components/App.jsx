59958690b6c02628974e2a1946d37940f28ca56a
Sasha Maslak <95244349+SashaMaslak@users.noreply.github.com>
2022-09-22 10:16:22 +0300

    complete

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,6 @@
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { addContact, deleteContact, filterContact } from 'redux/store';
 import { theme } from 'theme';
 import { Box } from './Box';
 import { Section } from './Section/Section';
@@ -9,41 +11,54 @@ import { Filter } from 'components/Filter/Filter';
 // helpers
 import { filterContacts } from 'helpers/filterContacts';
 
-const initialContacts = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
+// const initialContacts = [
+//   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+//   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+//   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+//   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+// ];
 
 export function App() {
-  const [contacts, setContacts] = useState(
-    JSON.parse(window.localStorage.getItem('contacts')) ?? initialContacts
-  );
-  const [filter, setFilter] = useState('');
+  const dispatch = useDispatch();
+  const contacts = useSelector(state => state.contacts);
+  const filter = useSelector(state => state.filter);
+
+  // const [contacts, setContacts] = useState(
+  //   JSON.parse(window.localStorage.getItem('contacts')) ?? initialContacts
+  // );
+  // const [filter, setFilter] = useState('');
 
   useEffect(() => {
     window.localStorage.setItem('contacts', JSON.stringify(contacts));
   }, [contacts]);
 
+  // const handleAddContact = contact => {
+  //   if (contacts.some(cont => cont.name === contact.name)) {
+  //     alert('Contact already exist');
+  //     return;
+  //   }
+  //   setContacts(prev => [...prev, contact]);
+  // };
+
   const handleAddContact = contact => {
-    if (contacts.some(cont => cont.name === contact.name)) {
-      alert('Contact already exist');
-      return;
-    }
-    setContacts(prev => [...prev, contact]);
+    console.log(contact);
+    dispatch(addContact(contact));
   };
 
+  // const handleDeleteContact = id => {
+  //   setContacts(prev => prev.filter(contact => contact.id !== id));
+  // };
+
   const handleDeleteContact = id => {
-    setContacts(prev => prev.filter(contact => contact.id !== id));
+    dispatch(deleteContact(id));
   };
 
   const setFilterValue = ({ target: { value } }) => {
-    setFilter(value);
+    dispatch(filterContact(value));
   };
 
   const filteredContacts = filterContacts(contacts, filter);
-  console.log(filter);
+
   return (
     <Box
       as={theme.as.s}
